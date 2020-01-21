abed79b5877c157cc89f3013b8233b80a128425a
MBezzubik <m.bezzubik@gmail.com>
2020-01-21 14:44:59 +0000

    merging

--- js/app.js
+++ js/app.js
@@ -9,16 +9,45 @@ var vm = new Vue({
     el: "#app",
     data: {
         courses: [{
-            "courseName": "vue crash course",
-            "location": "hendon",
-            "price": "130",
+            "courseName": "Vue Crash Course",
+            "location": "Hendon",
+            "price": "99",
             "images": [
-                "./img/courses/vue1.png",
-                "./img/courses/vue2.png",
-                "./img/courses/vue3.png",
-                "./img/courses/vue4.png",
+                "./img/courses/vue1.png"
+            ]
+        },
+        {
+            "courseName": "HTML Basics",
+            "location": "Ealing",
+            "price": "89",
+            "images": [
+                "./img/courses/vue2.png"
+            ]
+        },
+        {
+            "courseName": "Advanced JavaScript",
+            "location": "Barnes",
+            "price": "119",
+            "images": [
+                "./img/courses/vue3.png"
+            ]
+        },
+        {
+            "courseName": "Learning PHP",
+            "location": "Manchester",
+            "price": "59",
+            "images": [
+                "./img/courses/vue4.png"
+            ]
+        },
+        {
+            "courseName": "CSS in Practice",
+            "location": "Leicester",
+            "price": "74.99",
+            "images": [
                 "./img/courses/vue5.png"
             ]
-        }]
+        }
+    ]
     }
 });
