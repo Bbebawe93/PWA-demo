230debf8e9c92586b90bc964d0bc392c20c14514
unknown <bbebawe93@gmail.com>
2020-01-21 13:07:13 +0000

    jh

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // register service worker if supported in browser 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('PWA-demo/sw.js')
+    navigator.serviceWorker.register('/sw.js')
         .then((regObject) => console.log('[Service Worker] Registered', regObject))
         .catch((error) => console.log('[Service Worker] Not Registered', error))
 }
